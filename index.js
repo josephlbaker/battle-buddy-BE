109d8a612d085c712acd17afbb894a0055e4f332
josephlbaker <joelucasbaker@gmail.com>
2019-04-24 19:02:34 -0700

    updated routes

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/battle-buddy-BE
   { useNewUrlParser: true })
 
 module.exports = {
-  User: require("../models/user"),
-  Post: require("../models/post"),
-  Event: require("../models/event"),
-  Comment: require("../models/comment"),
-  Like: require("../models/like")
+  User: require("models/User"),
+  Post: require("models/Post"),
+  Event: require("models/Event"),
+  Comment: require("models/Comment"),
+  Like: require("models/Like")
 };
