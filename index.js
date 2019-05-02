7e68769aa806e3ee4c720ea55fcdd19b24415969
josephlbaker <joelucasbaker@gmail.com>
2019-05-02 12:59:28 -0700

    attempting to deploy

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/battle-buddy-BE
   { useNewUrlParser: true })
 
 module.exports = {
-  User: require("../models/User"),
-  Post: require("../models/Post"),
-  Event: require("../models/Event"),
-  Comment: require("../models/Comment"),
-  Like: require("../models/Like")
+  User: require("models/User"),
+  Post: require("models/Post"),
+  Event: require("models/Event"),
+  Comment: require("models/Comment"),
+  Like: require("models/Like")
 };
